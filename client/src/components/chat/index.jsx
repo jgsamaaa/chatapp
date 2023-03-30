90458a0ef8d8ce54b23cfb750c960971e1994c98
gabriel <jamesgabrieldumaug12@gmail.com>
2023-03-30 10:01:16 -0600

    chatbox

--- client/src/components/chat/index.jsx
+++ client/src/components/chat/index.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import {
+  UseMultiChatLogic,
+  MultiChatSocket,
+  MultiChatWindow,
+  useMultiChatLogic,
+} from 'react-chat-engine-advanced';
+
+import Header from '../../components/customHeader';
 
 const Chat = () => {
-  return <div>Chat</div>;
+  const chatProps = useMultiChatLogic(
+    import.meta.env.VITE_PROJECT_ID,
+    'test',
+    '12345'
+  );
+
+  return (
+    <div style={{ flexBasis: '100%' }}>
+      <MultiChatSocket {...chatProps} />
+      <MultiChatWindow
+        {...chatProps}
+        style={{ height: '100vh' }}
+        renderChatHeader={(chat) => <Header chat={chat} />}
+      />
+    </div>
+  );
 };
 
 export default Chat;
